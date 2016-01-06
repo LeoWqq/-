bd220b76a9e055ff53c89d849ce16a6379caac4d
doubletong <service@heiniaozhi.cn>
2016-01-06 12:04:25 +0800

    doubletong
    Signed-off-by: doubletong <service@heiniaozhi.cn>

---
+++ YDM.BussinessInterface/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YDM.BussinessInterface
+{
+    public class Class1
+    {
+    }
+}
